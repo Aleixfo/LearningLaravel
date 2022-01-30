382960031d86166c8c21046a5957485a754d49df
Aleixfo <afont15182@alumnes.iesmanacor.cat>
2022-01-30 22:37:22 +0100

    laravel blog

---
+++ Laravel/blog/resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <article>
+
+        <h1><a href="/posts/my-first-post"> My first post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum maxime ea ut odit libero commodi labore quam saepe, laborum doloremque omnis quod voluptates perspiciatis, praesentium vel, voluptatem vero pariatur suscipit in odio reiciendis. Quod sequi quam dicta animi porro assumenda totam fugiat perferendis qui reprehenderit temporibus odio, dolorem quis magni?
+        </p>
+
+
+
+
+    </article>
+
+    <article>
+
+        <h1><a href="/post"> My seconds post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum maxime ea ut odit libero commodi labore quam saepe, laborum doloremque omnis quod voluptates perspiciatis, praesentium vel, voluptatem vero pariatur suscipit in odio reiciendis. Quod sequi quam dicta animi porro assumenda totam fugiat perferendis qui reprehenderit temporibus odio, dolorem quis magni?
+        </p>
+
+
+
+
+    </article>
+    
+</body>
+</html>
--- Laravel/blog/routes/web.php
+++ Laravel/blog/routes/web.php
@@ -13,8 +13,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* AQUI ES DEFINEIXEN LES RUTES DEL LLOC WEB*/
+
 /* AIXO S'ANOMENA UNA FACHADA, EN AQUEST CAS LA FACHADA "ROUTE"
 I UTILITZA EL METODE GET */
+
+/* '/' -> el mappeig de la ruta
+    return view() -> Retorna una vista anomanada 'welcome' */
+
+    /*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}', function ($slug) {
+
+    /* file_get_contents(__DIR__) ens dona la ruta a aquest fitxer. */
+    $rutapost = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
+    
+    if (! file_exists($rutapost)){
+        dd('file does not exits');
+    }
+
+
+    return view('post', [
+        'post' => $rutapost
+    ]);
+});
+
+/* RETORNAR STRING 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+RETORNAR JSON 
+Route::get('/json', function () {
+    return ['foo' => 'bar'];
+});
+
+Route::get('/home', function () {
+    return view('index');
+});
+
+*/
